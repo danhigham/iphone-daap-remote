46eddb5b1187dfffa6b601606dfeda34808f25c6
Dan Higham <danhigham@danhigham-laptop.(none)>
2009-03-05 23:24:30 +0000

    Got rails app to stream to a holding directory, create a playlist and start mplayer!

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -2,23 +2,63 @@ include BrowseHelper
 
 class BrowseController < ApplicationController
   def index
+    @artists = get_music_index
+    @groups = @artists.collect { |x| x.name[/^./] }.uniq.sort
+  end    
+        
+  def play
+    stream_path = '/tmp/stream'
+    
+    playlist = params[:id].gsub('|', '.').split(',')
+    out_playlist = playlist.collect { |x|  "#{stream_path}#{File::SEPARATOR}#{x}\r\n" }
+        
+    FileUtils::mkdir_p(stream_path)
+    FileUtils.rm_r Dir.glob("#{stream_path}/*")
+
+    File.open("#{stream_path}#{File::SEPARATOR}playlist", "wb") { |file|
+      file.write out_playlist } 
+      
+    daap = Net::DAAP::Client.new('localhost')
+
+    daap.connect do |dsn|    
+      #Write out first song
+      db = daap.databases[0].id
+      
+      song = playlist.first
+      song_url = "databases/#{db}/items/#{song}"
+      filename = "#{stream_path}#{File::SEPARATOR}#{song}"
+
+      File.open(filename, "wb") { |file|
+        daap.get_song(song_url) { |x|
+          #FileUtils::mkdir_p(path)
+          file.write x
+        } 
+      }    
+    
+      pid = `ps a -o pid,comm | grep mplayer`.scan(/\d+/).to_s
+      `kill -9 #{pid}` if !pid.empty?
+      
+      # Start playing playlist 
+      #Open3::popen3 "mplayer -playlist #{stream_path}#{File::SEPARATOR}playlist &"
+      system("mplayer -playlist #{stream_path}#{File::SEPARATOR}playlist &")
             
-    daap = Net::DAAP::Client.new('192.168.1.5')
+      if (playlist.length > 1)
+        (1..playlist.length-1).each { |z|
+          song = playlist[z]
+          song_url = "databases/#{db}/items/#{song}"
+          filename = "#{stream_path}#{File::SEPARATOR}#{song}"
 
-    daap.connect do |dsn|        
-      daap.databases.each do |db|
-        @artists = db.artists
-      end
-
-      @groups = @artists.collect { |x| x.name[/^./] }.uniq.sort
-
-    end    
+          File.open(filename, "wb") { |file|
+        
+            daap.get_song(song_url) { |x|
+              #FileUtils::mkdir_p(path)
+              file.write x
+            } 
+          }
+        }
+      end 
+    end
     
-  end
-  
-  def artist
-    artist_name = params[:id]
-    
-    
+    render :text => playlist.inspect
   end
 end
--- app/helpers/browse_helper.rb
+++ app/helpers/browse_helper.rb
@@ -4,15 +4,113 @@ require 'fileutils'
 require 'ruby-debug'
 
 module BrowseHelper
+  def get_music_index
+      daap = Net::DAAP::Client.new('localhost')
+
+      daap.connect do |dsn|        
+        daap.databases.each do |db|
+          return db.artists
+        end
+      end   
+  end
+  
+  def download_song
+    
+  end
 end
 
 
 module Net
   module DAAP
+  # This class contains a database found on an iTunes server.
+    class Database
+      attr_reader :persistentid, :name, :containercount, :id, :itemcount
+      attr_blockreader :songs, :artists, :albums
+
+      @@SONG_ATTRIBUTES = %w{ dmap.itemid 
+                              dmap.itemname 
+                              dmap.persistentid
+                              daap.songalbum
+                              daap.songartist 
+                              daap.songformat
+                              daap.songsize 
+                              daap.songtracknumber 
+                              daap.songdataurl }
+
+      def initialize(args)
+        @persistentid   = args['dmap.persistentid']
+        @name           = args['dmap.itemname']
+        @containercount = args['dmap.containercount']
+        @id             = args['dmap.itemid']
+        @itemcount      = args['dmap.itemcount']
+        @daap           = args[:daap]
+        @songs          = []
+        @artists        = []
+        @albums         = []
+        load_songs
+      end
+
+# Returns the playlists associated with this database
+      def playlists
+        url = "databases/#{@id}/containers?meta=dmap.itemid,dmap.itemname,dmap.persistentid,com.apple.itunes.smart-playlist"
+        res = @daap.do_get(url)
+
+        listings = @daap.dmap.find(res, "daap.databaseplaylists/dmap.listing")
+
+        playlists = []
+        @daap.unpack_listing(listings) do |value|
+          playlist = Playlist.new( value.merge( 
+                                    :daap     => @daap,
+                                    :db       => self ))
+          if block_given?
+            yield playlist
+          else
+            playlists << playlist
+          end
+        end
+        playlists
+      end
+
+      private
+      def load_songs
+        path = "databases/#{@id}/items?type=music&meta="
+        path += @@SONG_ATTRIBUTES.join(',')
+
+        listings = @daap.dmap.find(@daap.do_get(path),
+                             "daap.databasesongs/dmap.listing")
+        artist_hash = {}
+        album_hash  = {}
+        @daap.unpack_listing(listings) do |value|
+        
+          artist  = artist_hash[value['daap.songartist']] ||= Artist.new(value)
+          album   = album_hash[value['daap.songalbum']]   ||= Album.new(
+                              :name   => value['daap.songalbum'],
+                              :artist => artist )
+
+          song = Song.new(  value.merge(
+                            :daap      => @daap,
+                            :db        => self,
+                            :artist    => artist,
+                            :album     => album))
+
+          album.songs   << song
+          artist.songs  << song
+          @songs        << song
+        end
+
+        # Add each album to its artist
+        album_hash.each_value do |value|
+          value.artist.albums << value
+          @albums << value
+        end
+
+        artist_hash.each_value { |v| @artists << v }
+      end
+    end
 # This class contains song information returned from the DAAP server.
     class Song
       include Comparable
-      attr_reader :size, :album, :name, :artist, :format, :persistentid, :id
+      attr_reader :size, :album, :name, :artist, :format, :persistentid, :id, :tracknum, :dataurl
       attr_accessor :path, :file
 
       alias :to_s :name
@@ -26,6 +124,8 @@ module Net
         @format         = args['daap.songformat']
         @persistentid   = args['dmap.persistentid']
         @id             = args['dmap.itemid']
+        @tracknum       = args['daap.songtracknumber']
+        @dataurl        = args['daap.songdataurl']
         @daap           = args[:daap]
         @db             = args[:db]
         @path = [@artist.name, @album.name].collect { |name|
